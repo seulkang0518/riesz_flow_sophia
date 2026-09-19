f36c215e313a7fe7fa27cc4d8d8a7503a9ebcaf1
kangsophia518 <seulkang0518@gmail.com>
2026-09-19 12:58:55 -0700

    Update imagenet256_laplace.sh

--- scripts/eval_fid/imagenet256_laplace.sh
+++ scripts/eval_fid/imagenet256_laplace.sh
@@ -99,7 +99,7 @@ echo "Output root:      $OUT_ROOT"
 echo "MASTER_PORT:      $MASTER_PORT"
 echo "CUDA_VISIBLE_DEVICES: ${CUDA_VISIBLE_DEVICES:-unset}"
 
-for STEPNUM in 00012000; do
+for STEPNUM in 00014000; do
   CKPT="$CKPT_DIR/state_${STEPNUM}.pt"
 
   if [[ ! -f "$CKPT" ]]; then
